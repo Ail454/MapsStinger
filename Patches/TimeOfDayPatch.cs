44d99684e82746eefbcd78ea42e5a0e4822820cb
Ail454 <oignion.46@gmail.com>
2025-02-22 16:27:59 +0100

    Add project files.

---
+++ Patches/TimeOfDayPatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Logging;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using UnityEngine;
+
+namespace MapsStinger.Patches
+{
+    [HarmonyPatch(typeof(TimeOfDay))]
+    internal class TimeOfDayPatch
+
+    {
+        private const string modGUID = "Oignion.MapsStinger";
+        internal ManualLogSource mls;
+
+        [HarmonyPatch("PlayTimeMusicDelayed")]
+        [HarmonyPrefix]
+        static void OverrideAudio(TimeOfDay __instance)
+        {
+            if (StartOfRound.Instance.currentLevelID == 7)
+            {
+                __instance.TimeOfDayMusic.clip = MapsStinger.SoundFX[1];
+                MapsStinger.mls.LogInfo("Dine Music");
+            }
+            else
+            {
+                if (StartOfRound.Instance.currentLevelID == 6) 
+                {
+                    __instance.TimeOfDayMusic.clip = MapsStinger.SoundFX[2];
+                    MapsStinger.mls.LogInfo("Rend Music");
+                }
+                else 
+                    {
+                        __instance.TimeOfDayMusic.clip = TimeOfDay.Instance.TimeOfDayMusic.clip;
+                    MapsStinger.mls.LogInfo("Base music");
+                    }
+                    
+            }
+            
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MapsStinger.Patches;
+using UnityEngine;
+
+namespace MapsStinger
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MapsStinger : BaseUnityPlugin
+    {
+        private const string modGUID = "Oignion.MapsStinger";
+        private const string modName = "MapsStinger";
+        private const string modVersion = "1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static MapsStinger Instance;
+        internal static List<AudioClip> SoundFX;
+        internal static AssetBundle Bundle;
+
+        internal new static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("It loaded");
+
+            harmony.PatchAll(typeof(TimeOfDayPatch));
+
+            mls = Logger;
+
+            SoundFX = new List<AudioClip>();
+            string FolderLocation = Instance.Info.Location;
+            FolderLocation = FolderLocation.TrimEnd("MapsStinger.dll".ToCharArray());
+            Bundle = AssetBundle.LoadFromFile(FolderLocation + "asset");
+            if(Bundle != null)
+            {
+                mls.LogInfo("asset bundle loaded");
+                SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
+            }
+            else
+            {
+                mls.LogError("Uh the asset bundle haven't loaded");
+            }
+
+        }
+    }
+}
